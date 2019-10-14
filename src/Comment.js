a48670eaeaf10a3ef0f89d8749faef9672166bb1
Alan Krajina <alankrajina88@gmail.com>
2019-10-14 18:01:37 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -4,7 +4,7 @@ export default class Comment extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
+      <div>
         {/* your conditional code here! */}
       </div>
     )
